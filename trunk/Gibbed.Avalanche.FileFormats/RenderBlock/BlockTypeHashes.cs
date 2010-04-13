3c8dfb31a9cbc7ea8176121ffb74c71321dbbc39
rick <rick@d840cbf1-3b29-df11-ba30-0030489379b8>
2010-04-13 04:42:45 +0000

    CarPaint + DeformableWindow support for RbModel stuff.

---
+++ trunk/Gibbed.Avalanche.FileFormats/RenderBlock/BlockTypeHashes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gibbed.Avalanche.FileFormats.RenderBlock
+{
+    public enum BlockTypeHashes : uint
+    {
+        CarPaint = 0xCD931E75,
+        SkinnedGeneral = 0x5E657F20,
+    }
+}
---
+++ trunk/Gibbed.Avalanche.ModelViewer/Renderers/DeformableWindowRenderer.cs
@@ -0,0 +1,80 @@
+﻿using System.IO;
+using Gibbed.Avalanche.FileFormats.RenderBlock;
+using Microsoft.Xna.Framework.Graphics;
+
+
+namespace Gibbed.Avalanche.ModelViewer.Renderers
+{
+    internal class DeformableWindowRenderer : GenericBlockRenderer<DeformableWindow>
+    {
+        #region Vertex Elements
+        public static VertexElement[] VertexElements =
+        {
+            // first one "should" be Vertex4
+            new VertexElement(0, 0, VertexElementFormat.Vector3, VertexElementMethod.Default, VertexElementUsage.Position, 0),
+            new VertexElement(0, 16, VertexElementFormat.NormalizedShort4, VertexElementMethod.Default, VertexElementUsage.TextureCoordinate, 1),
+            new VertexElement(0, 24, VertexElementFormat.Vector4, VertexElementMethod.Default, VertexElementUsage.TextureCoordinate, 2),
+            new VertexElement(0, 40, VertexElementFormat.Vector2, VertexElementMethod.Default, VertexElementUsage.TextureCoordinate, 0),
+        };
+        #endregion
+
+        private Texture TextureDif;
+        private Texture TextureNrm;
+        private VertexDeclaration VertexDeclaration;
+
+        public override void Setup(GraphicsDevice device, DeformableWindow block, string basePath)
+        {
+            this.VertexDeclaration = new VertexDeclaration(device, VertexElements);
+
+            string texturePath;
+
+            texturePath = Path.Combine(basePath, block.Textures[0]);
+            if (File.Exists(texturePath) == false)
+            {
+                this.TextureDif = null;
+            }
+            else
+            {
+                this.TextureDif = Texture.FromFile(device, texturePath);
+            }
+
+            texturePath = Path.Combine(basePath, block.Textures[1]);
+            if (File.Exists(texturePath) == false)
+            {
+                this.TextureNrm = null;
+            }
+            else
+            {
+                this.TextureNrm = Texture.FromFile(device, texturePath);
+            }
+        }
+
+        public override void Render(GraphicsDevice device, DeformableWindow block)
+        {
+            VertexBuffer vertices;
+            int vertexSize;
+
+            device.VertexDeclaration = this.VertexDeclaration;
+            vertices = new VertexBuffer(
+                device,
+                block.Vertices.Count * 48,
+                BufferUsage.WriteOnly);
+            vertices.SetData(block.Vertices.ToArray());
+
+            device.Vertices[0].SetSource(vertices, 0, 48);
+
+            var indices = new IndexBuffer(
+                    device,
+                    typeof(short),
+                    block.Faces.Count,
+                    BufferUsage.WriteOnly);
+            indices.SetData(block.Faces.ToArray(), 0, block.Faces.Count);
+            device.Indices = indices;
+
+            device.Textures[0] = this.TextureDif;
+            device.Textures[1] = this.TextureNrm; // not "working" yet (needs shader~)
+
+            device.DrawIndexedPrimitives(PrimitiveType.TriangleList, 0, 0, block.Faces.Count, 0, block.Faces.Count / 3);
+        }
+    }
+}

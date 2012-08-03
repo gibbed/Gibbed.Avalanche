ecaf89f8815eda59cabe7403a5606960b7945024
rick <rick@d840cbf1-3b29-df11-ba30-0030489379b8>
2012-08-03 02:12:31 +0000

    SaveFile code cleanup.

--- trunk/Gibbed.Avalanche.FileFormats/Save/Section.cs
+++ trunk/Gibbed.Avalanche.FileFormats/Save/Section.cs
@@ -1,7 +1,27 @@
-﻿using System;
+﻿/* Copyright (c) 2012 Rick (rick 'at' gibbed 'dot' us)
+ * 
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ * 
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ * 
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would
+ *    be appreciated but is not required.
+ * 
+ * 2. Altered source versions must be plainly marked as such, and must not
+ *    be misrepresented as being the original software.
+ * 
+ * 3. This notice may not be removed or altered from any source
+ *    distribution.
+ */
+
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace Gibbed.Avalanche.FileFormats.Save
 {
@@ -32,7 +52,7 @@ namespace Gibbed.Avalanche.FileFormats.Save
             if (size >= 0)
             {
                 return this.Entries.ContainsKey(id) &&
-                    this.Entries[id].Length == size;
+                       this.Entries[id].Length == size;
             }
 
             return this.Entries.ContainsKey(id);
@@ -70,7 +90,11 @@ namespace Gibbed.Avalanche.FileFormats.Save
 
         public void PutB8(short id, bool value)
         {
-            this.PutData(id, new[] { value == true ? (byte)1 : (byte)0 });
+            this.PutData(id,
+                         new[]
+                         {
+                             value == true ? (byte)1 : (byte)0
+                         });
         }
 
         public byte? GetU8(short id)
@@ -85,7 +109,11 @@ namespace Gibbed.Avalanche.FileFormats.Save
 
         public void PutU8(short id, byte value)
         {
-            this.PutData(id, new[] { value });
+            this.PutData(id,
+                         new[]
+                         {
+                             value
+                         });
         }
 
         public int? GetS32(short id)
